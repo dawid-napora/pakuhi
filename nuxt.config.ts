c36bf6c479c7aa60f47a6421c56f9df6273294b3
Dawid Napora <dawid@napora.dev>
2023-09-10 10:49:48 +0200

    fix: add runtime config for auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: ["@sidebase/nuxt-auth", "@nuxtjs/tailwindcss"],
+
+  runtimeConfig: {
+    secret: process.env.AUTH_SECRET,
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    },
+  },
+
+  auth: {
+    origin: process.env.AUTH_ORIGIN,
+  },
 });
